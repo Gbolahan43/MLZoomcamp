7dcf2aa970f354f952fed3b61c8de4411d528ac4
Gbolahan43 <gbolahanbasit43@gmail.com>
2024-10-28 10:25:08 +0100

    week 5

---
+++ Assigments/week 5/ping.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask('ping')
+@app.route('/ping', methods = ['GET'])
+def ping():
+    return ('hello excellus says pong')
+
+if __name__=="__main__":
+    app.run(debug=True, host='0.0.0.0', port = 9696)
