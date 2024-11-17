e7be5192308fe514ad8de113c427b8af16e7ad15
Gbolahan43 <gbolahanbasit43@gmail.com>
2024-11-17 17:56:09 +0100

    deploying model using flask

---
+++ Assigments/week 7/predict.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+import pickle
+from flask import request
+
+
+with open('rfcmodel.bin', 'rb') as f_in:
+    model = pickle.load(f_in)
+
+with open('dv.bin', 'rb') as dict_vect:
+    dv = pickle.load(dict_vect)
+
+app = Flask('predict')
+
+@app.route('/predict', methods = ['POST'])
+
+def predict():
+    data = request.get_json()
+    
+    X = dv.transform([data])
+    y_pred = model.predict_proba(X)[0,1]
+    rain = y_pred >= 0.5
+
+    result = {
+        'rain' : bool(rain),
+        'Rain Probability' : float(y_pred)
+    }
+    return jsonify(result)
+
+if __name__=="__main__":
+    app.run(debug=True, host='0.0.0.0', port = 9696)
